11f147c8e2c185ce30d1f18676b5022a777608a0
isadorarat <dorinha.bailarina@gmail.com>
2024-10-11 10:59:34 -0300

    protegendo route

--- interface/src/components/Navegacao/Navegacao.jsx
+++ interface/src/components/Navegacao/Navegacao.jsx
@@ -1,33 +1,28 @@
-import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/esm/Button';
+import AuthRequests from '../../fetch/AuthRequests';
+import { useState, useEffect } from 'react';
 
 function Navegacao() {
-
-    // Gerencia o estado de autenticação
+    // estado para controlar se o usuário está logado ou não
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [nomeUsuario, setNomeUsuario] = useState('');
 
-    // Verifica o token de autenticação no localStorage
+    /**
+    * Verifica a autenticação do usuário
+    */
     useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsAuthenticated(true);
+        const isAuth = localStorage.getItem('isAuth');  // recupera o valor de autenticação do localstorage
+        const token = localStorage.getItem('token');  // recupera o token do localstorage
+        if (isAuth && token && AuthRequests.checkTokenExpiry()) {  // verifica se isAuth é true, verifica se o token existe e verifica se o token é válido
+            setIsAuthenticated(true);  // caso o token seja válido, seta o valor de autenticação para true
+            setNomeUsuario(localStorage.getItem('username'));
+        } else {
+            setIsAuthenticated(false);  // caso o token seja inválido, seta o valor de autenticação para false
         }
     }, []);
-
-    // Lida com o logout
-    const handleLogout = () => {
-        localStorage.clear();  // Remove todos os itens do localStorage
-        setIsAuthenticated(false);  // Atualiza o estado de autenticação
-        window.location.href = '/';  // Redireciona para a página inicial
-    };
-
-    // Lida com o login
-    const handleLogin = () => {
-        window.location.href = '/login';  // Redireciona para a página de login
-    };
 
     const estiloNavbar = {
         backgroundColor: 'var(--primaryColor)',
@@ -35,20 +30,31 @@ function Navegacao() {
 
     const estiloNavOptions = {
         color: 'var(--fontColor)',
+        marginRight: '15px',
+    }
+
+    const logout = () => {
+        AuthRequests.removeToken();
     }
 
     return (
         <>
             <Navbar style={estiloNavbar}>
                 <Container>
+                    {/* a opção Home é renderizada para todos os usuários, independente de estarem autenticados ou não */}
                     <Navbar.Brand href="/" style={estiloNavOptions}>Home</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/pessoas" style={estiloNavOptions}>Pessoas</Nav.Link>
-                    </Nav>
                     {isAuthenticated ? (
-                        <Button onClick={handleLogout} variant='light'>Logout</Button>
+                        // renderiza as opções de navegação para usuário autenticado
+                        <>
+                            <Nav className="me-auto">
+                                <Nav.Link href="/pessoas" style={estiloNavOptions}>Pessoas</Nav.Link>
+                            </Nav>
+                            <Nav.Item style={estiloNavOptions}>Bem-vindo, {nomeUsuario}</Nav.Item>
+                            <Button variant='light' onClick={logout}>Sair</Button>
+                        </>
                     ) : (
-                        <Button onClick={handleLogin} variant='light'>Login</Button>
+                        // renderiza as opções de navegação para usuário não autenticado
+                        <Button href='/login' variant='light'>Login</Button>
                     )}
                 </Container>
             </Navbar>
--- interface/src/fetch/AuthRequests.js
+++ interface/src/fetch/AuthRequests.js
@@ -2,13 +2,13 @@
  * Classe para lidar com autenticação
  */
 class AuthRequests {
-
+    
     /**
      * Construtor das rotas e do endereço do servidor
      */
     constructor() {
         // endereço do servidor
-        this.serverUrl = 'http://localhost:3333';
+        this.serverUrl = 'http://localhost:3334';
         // rota do servidor
         this.routeLogin = '/login';
     }
@@ -18,7 +18,7 @@ class AuthRequests {
      * @param {*} login - email e senha
      * @returns **true** caso sucesso, **false** caso erro
      */
-    async login(login) {
+    async login(login) {       
         try {
             // faz a requisição POST ao servidor...
             const response = await fetch(`${this.serverUrl}${this.routeLogin}`, {
@@ -29,7 +29,7 @@ class AuthRequests {
                 // passando as informações de login no corpo da requisição
                 body: JSON.stringify(login)
             });
-
+            
             // Verifica alguma falha na comunicação
             if (!response.ok) {
                 console.log('Erro na autenticação');
@@ -37,12 +37,12 @@ class AuthRequests {
             }
             // caso a requisição seja bem sucedida, armazena a resposta em uma constante
             const data = await response.json();
-            console.log(data);
+            console.log( data );
 
             // verifica se o atributo auth da resposta tem o valor TRUE, se tiver é porque a autenticação teve sucesso
             if (data.auth) {
                 // persistem o token, o nome e o id do professor no localstorage
-                this.persistToken(data.token, data.usuario.nome, data.usuario.id_usuario);
+                this.persistToken(data.token, data.usuario.nome, data.usuario.id_usuario, data.auth);
             }
 
             // retorna a resposta da requisição a quem chamou a função
@@ -60,27 +60,31 @@ class AuthRequests {
      * @param {*} username - nome usuário recebido do servidor
      * @param {*} idUsuario - idUsuario recebido do servidor
      */
-    persistToken(token, username, idUsuario) {
+    persistToken(token, username, idPessoa, isAuth) {
         // adiciona o token no localstorade com o apelido de token
-        localStorage.setItem('token', token);
+        localStorage.setItem('token', token);  // -> armazena o token no localStorage e coloca o 'apelido' de token
         // adiciona o nome de usuário no localstorade com o apelido de username
-        localStorage.setItem('username', username);
+        localStorage.setItem('username', username);  // -> armazena o username no localStorage e coloca o 'apelido' de username 
         // adiciona o id da pessoa no localstorade com o apelido de idPessoa
-        localStorage.setItem('idUsuario', idUsuario);
+        localStorage.setItem('idPessoa', idPessoa);  // -> armazena o idPessoa no localStorage e coloca o 'apelido' de idPessoa
+        // adiciona o valor de autenticação no localstorade com o apelido de isAuth
+        localStorage.setItem('isAuth', isAuth);  // -> armazena o estado da autenticação (true, false) no localStorage e coloca o 'apelido' de isAuth
     }
 
     /**
-     * Remove o token no localStorage
+     * Remove as informações do localStorage
      */
     removeToken() {
         // remove o token do localstorade
-        localStorage.removeItem('token');
+        localStorage.removeItem('token');  // -> remove o 'apelido' de token do localStorage
         // remove o username do localstorage
-        localStorage.removeItem('username');
+        localStorage.removeItem('username');  // -> remove o 'apelido' de username do localStorage
         // remove o idPessoa do localstorage
-        localStorage.removeItem('idPessoa');
-        // redireciona o usuário para a página Home
-        window.location.href = '/';
+        localStorage.removeItem('idPessoa');  // -> remove o 'apelido' de idPessoa do localStorage
+        // remove o isAuth do localstorage
+        localStorage.removeItem('isAuth');  // -> remove o 'apelido' de isAuth do localStorage
+        // redireciona o usuário para a página de login
+        window.location.href = '/login';
     }
 
     /**
@@ -90,7 +94,7 @@ class AuthRequests {
     checkTokenExpiry() {
         // recupera o valor do token no localstorage
         const token = localStorage.getItem('token');
-
+        
         // verifica se o valor é diferente de vazio
         if (token) {
             // recupera a data de expiração do token
